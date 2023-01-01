f532ebb19c64cfa08c4e04e2297cf68dcdb21d1a
bruno <bruno.resendesantos45@gmail.com>
2023-01-01 12:00:27 -0300

    Feat: Added sorting of tailwind

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -1,18 +1,20 @@
-import type { FC } from 'react';
-import { BPM } from './Bpm';
-import { Measure } from './Measure';
-import { PlayButton } from './PlayButton';
-import { Slider } from './Slider';
+import type { FC } from "react";
+import { BPM } from "./Bpm";
+import { Measure } from "./Measure";
+import { PlayButton } from "./PlayButton";
+import { Slider } from "./Slider";
 
 interface MetronomeProps {}
 
 export const Metronome: FC<MetronomeProps> = ({}) => {
   return (
-    <div>
-      <BPM />
-      <Slider />
-      <PlayButton />
-      <Measure />
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col shadow-md">
+        <BPM />
+        <Slider />
+        <PlayButton />
+        <Measure />
+      </div>
     </div>
   );
 };
