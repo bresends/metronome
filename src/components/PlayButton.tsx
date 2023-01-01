9991e20578df70e2c18caa8854b222a23c31eb34
bruno <bruno.resendesantos45@gmail.com>
2023-01-01 18:43:45 -0300

    Feat: Added Sounds

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,13 +1,20 @@
-import type { FC } from "react";
+import { FC, useEffect } from "react";
+import audio from "../assets/click1.mp3";
 
 interface PlayButtonProps {}
 
 export const PlayButton: FC<PlayButtonProps> = ({}) => {
+  const player = new Audio(audio);
+
   return (
     <>
-      <button className="my-4 flex h-20 w-20 items-center justify-center rounded-full bg-baseRed text-center text-sm text-white transition duration-200 hover:scale-110 hover:bg-red-400">
+      <button
+        onClick={() => player.play()}
+        className="my-4 flex h-20 w-20 items-center justify-center rounded-full bg-baseRed text-center text-sm text-white transition duration-200 hover:scale-110 hover:bg-red-400"
+      >
         START
       </button>
+      <audio src={audio} className="bg-red-300" />
     </>
   );
 };
