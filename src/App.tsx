11442a8db8963180c3c2a5cf3e0a33fa3cfa360b
bruno <bruno.resendesantos45@gmail.com>
2023-01-01 11:23:40 -0300

    Feat: Added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { Metronome } from './components/Metronome';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Metronome />
     </div>
   );
 }
---
+++ src/components/Metronome.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+interface MetronomeProps {}
+
+export const Metronome: FC<MetronomeProps> = ({}) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
