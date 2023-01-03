d1e28883ad283fdecac1609e9b776f2503a9aaa2
bruno <bruno.resendesantos45@gmail.com>
2023-01-03 18:38:11 -0300

    Feat: Added time custom hook

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+
+import { useTimer } from "../hooks/useTimer";
 import { BPM } from "./Bpm";
 import { Measure } from "./Measure";
 import { PlayButton } from "./PlayButton";
@@ -9,6 +11,9 @@ interface MetronomeProps {}
 export const Metronome: FC<MetronomeProps> = ({}) => {
   const [tempo, setTempo] = useState(180);
   const [measure, setMeasure] = useState(4);
+
+  const { time } = useTimer(100);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex w-96 flex-col items-center justify-between">
@@ -16,6 +21,7 @@ export const Metronome: FC<MetronomeProps> = ({}) => {
         <Slider {...{ tempo, setTempo }} />
         <PlayButton />
         <Measure {...{ measure, setMeasure }} />
+        {(time / 1000).toFixed(1)}s
       </div>
     </div>
   );
